573af6b5afb60e55175c0211412c4534c7f12f80
平間翔大 <shotahirama@hiramashotanoMacBook-Pro.local>
2017-08-07 07:46:57 +0900

    add TEST_NEAR sample

--- src/mathtest.cpp
+++ src/mathtest.cpp
@@ -1,26 +1,21 @@
 #include <cmath>
-#include <iostream>
 #include <gtest/gtest.h>
+#include <iostream>
 
-double Deg2Rad(double deg)
-{
-    return M_PI*deg/180.0;
+double Deg2Rad(double deg) { return M_PI * deg / 180.0; }
+
+double Rad2Deg(double rad) { return 180.0 * rad / M_PI; }
+
+TEST(mathtest, angletest) {
+  EXPECT_EQ(Deg2Rad(180.0), M_PI);
+  EXPECT_EQ(Rad2Deg(M_PI), 180.0);
 }
 
-double Rad2Deg(double rad){
-    return 180.0*rad/M_PI;
+TEST(mathtest, powtest) {
+  EXPECT_EQ(pow(1, 0), 1);
+  EXPECT_EQ(pow(0, 1), 0);
+  EXPECT_EQ(pow(2, 2), 4);
+  EXPECT_EQ(pow(2, 3), 8);
 }
 
-TEST(mathtest, angletest)
-{
-    EXPECT_EQ(Deg2Rad(180.0),M_PI);
-    EXPECT_EQ(Rad2Deg(M_PI),180.0);
-}
-
-TEST(mathtest, powtest)
-{
-    EXPECT_EQ(pow(1,0),1);
-    EXPECT_EQ(pow(0,1),0);
-    EXPECT_EQ(pow(2,2),4);
-    EXPECT_EQ(pow(2,3),8);
-}
+TEST(mathtest, near_test) { EXPECT_NEAR(Rad2Deg(3.14), 180.0, 0.1); }
